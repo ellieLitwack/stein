6f14528a41f9d4cd1ce155d9b04feb0e5c76ff8e
Ellie Litwack <19858659+ellieLitwack@users.noreply.github.com>
2023-07-18 16:52:40 -0400

    updated home url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(name='stein',
 version='0.1',
 description='Stein provides a wrapper around Pint and zero overhead unit conversion factors. This allows for identifying unit errors during development and then removing the overhead of quantity objects during production.',
-url='#',
+url='https://github.com/ellieLitwack/stein',
 author='Ellie Litwack',
 install_requires=['pint'],
 author_email='',
